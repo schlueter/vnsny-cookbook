ed16c70d18d9882c290cd525cba2a0af132a20cb
B Schlueter <bs@bschlueter.com>
2014-10-12 00:28:13 -0400

    Control apache2 templates

--- recipes/apache_setup.rb
+++ recipes/apache_setup.rb
@@ -3,6 +3,24 @@ web_app node['project_data']['name'] do
   server_aliases [node['project_data']['hostname']]
   docroot "#{node['project_data']['workspace_dir']}/web"
   allow_override "All"
+  cookbook 'vnsny'
+end
+
+# Copied from the apache2::default recipe in order to override the file
+template 'apache2.conf' do
+  case node['platform_family']
+  when 'rhel', 'fedora', 'arch'
+    path "#{node['apache']['dir']}/conf/httpd.conf"
+  when 'debian'
+    path "#{node['apache']['dir']}/apache2.conf"
+  when 'freebsd'
+    path "#{node['apache']['dir']}/httpd.conf"
+  end
+  source   'apache2.conf.erb'
+  owner    'root'
+  group    node['apache']['root_group']
+  mode     '0644'
+  notifies :reload, 'service[apache2]'
 end
 
 ### a kludge since the name of the default site seems to have changed from 2.2 to 2.4?
--- recipes/vagrant_user.rb
+++ recipes/vagrant_user.rb
@@ -3,7 +3,6 @@ directory '/home/vagrant/.ssh' do
 end
 
 cookbook_file '/home/vagrant/.ssh/id_rsa' do
-  source 'id_rsa'
   mode 0600
   owner 'vagrant'
   group 'vagrant'
