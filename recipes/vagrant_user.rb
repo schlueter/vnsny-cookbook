db3b95d658e3f90c66295b5b41ce1d795a19d654
B Schlueter <bs@bschlueter.com>
2014-10-12 00:31:04 -0400

    Just touch .ssh/id_rsa file

--- recipes/vagrant_user.rb
+++ recipes/vagrant_user.rb
@@ -2,8 +2,9 @@ directory '/home/vagrant/.ssh' do
   mode 0700
 end
 
-cookbook_file '/home/vagrant/.ssh/id_rsa' do
+file '/home/vagrant/.ssh/id_rsa' do
   mode 0600
   owner 'vagrant'
   group 'vagrant'
+  action :touch
 end
