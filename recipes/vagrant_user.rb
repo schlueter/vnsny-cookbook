9e825c98f0065f6bd220e7ee4f51d9c7bc455f3a
B Schlueter <bs@bschlueter.com>
2014-10-12 22:33:56 -0400

    Use data bag item for id_rsa

--- recipes/vagrant_user.rb
+++ recipes/vagrant_user.rb
@@ -1,10 +1,12 @@
+data_bag_item = Chef::EncryptedDataBagItem.load 'secrets', 'id_rsa'
+
 directory '/home/vagrant/.ssh' do
   mode 0700
 end
 
 file '/home/vagrant/.ssh/id_rsa' do
+  content data_bag_item['private_key']
   mode 0600
   owner 'vagrant'
   group 'vagrant'
-  action :touch
 end
